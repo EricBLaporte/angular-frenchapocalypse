50d56447687b809991e0dc4b20f6659b9f5272ff
EricBLaporte <laporte.eric.b@gmail.com>
2020-03-30 19:19:30 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-frenchapocalypse
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-frenchapocalypse)
---
+++ src/app/top-hat/top-hat.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-top-hat',
+  templateUrl: './top-hat.component.html',
+  styleUrls: ['./top-hat.component.css']
+})
+export class TopHatComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
