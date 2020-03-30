3f23f35cc7fb128614365852651f2ff9f24ca7f8
EricBLaporte <laporte.eric.b@gmail.com>
2020-03-30 19:55:07 +0200

    3 files modified, 3 deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 declare var ol: any;
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular';
+
+export class AppComponent {
+    name = "Angular";
+  latitude: number = 2.335106;
+  longitude: number = 48.861191;
+
+  map: any;
+
+  ngOnInit() {
+    this.map = new ol.Map({
+      target: "map",
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM()
+        })
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([2.335106, 46.8]),
+        zoom: 5
+      })
+    });
+  }
 }
